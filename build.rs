f35d4e7a636e4846334a8cfd84ba647590bccedf
Alexander Polyakov <apolyakov@beget.com>
2017-05-15 14:34:15 +0300

    Commit original remote_protocol.x from libvirt

--- build.rs
+++ build.rs
@@ -1,5 +1,35 @@
 extern crate xdrgen;
 
+use std::process::Command;
+use std::fs::OpenOptions;
+use std::env;
+use std::path::PathBuf;
+use std::io::Write;
+
 fn main() {
     xdrgen::compile("virnetprotocol.x").unwrap();
+
+    // compiling remote_protocol.x is a bit more involved
+    // first process it with cpp to eval defines
+    let cpp = Command::new("/usr/bin/cpp")
+    // constants from libvirt-host.h
+        .arg("-DVIR_SECURITY_MODEL_BUFLEN=257")
+        .arg("-DVIR_SECURITY_LABEL_BUFLEN=4097")
+        .arg("-DVIR_SECURITY_DOI_BUFLEN=257")
+        .arg("remote_protocol.x")
+        .output().unwrap();
+
+    // then write output to temporarily file
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut path = PathBuf::from(out_dir);
+    path.push("remote_protocol.x");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&path).unwrap();
+    file.write_all(&cpp.stdout).unwrap();
+
+    // finally run xdrgen
+    let path_str = format!("{}", path.display());
+    xdrgen::compile(path_str).unwrap();
 }
