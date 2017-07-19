b4d0575eccff0bf3ecbeb3c0d587319a51a073d1
Alexander Polyakov <apolyakov@beget.com>
2017-07-19 18:47:39 +0300

    Hide migrate_begin

--- src/async.rs
+++ src/async.rs
@@ -560,14 +560,15 @@ impl<'a> DomainOperations<'a> {
         Box::new(self.client.request(request::remote_procedure::REMOTE_PROC_DOMAIN_SEND_KEY, pl).map(|resp| resp.into()))
     }
 
-    pub fn migrate_begin(&self, dom: &request::Domain, params: Vec<request::MigrationParam>, flags: request::DomainMigrateFlags::DomainMigrateFlags) -> LibvirtFuture<()> {
+    fn migrate_begin(&self, dom: &request::Domain, params: Vec<request::MigrationParam>, flags: request::DomainMigrateFlags::DomainMigrateFlags) -> LibvirtFuture<()> {
         let pl = request::MigrateBeginRequest::new(dom, params, flags);
         Box::new(self.client.request(request::remote_procedure::REMOTE_PROC_DOMAIN_MIGRATE_BEGIN3_PARAMS, pl).map(|resp| {
             println!("DEBUG RESP: {:?}", resp);
         }))
     }
 
-    pub fn migrate_perform(&self, dom: &request::Domain, uri: &str, params: Vec<request::MigrationParam>, flags: request::DomainMigrateFlags::DomainMigrateFlags) -> LibvirtFuture<()> {
+    /// Performs unmanaged migration
+    pub fn migrate(&self, dom: &request::Domain, uri: &str, params: Vec<request::MigrationParam>, flags: request::DomainMigrateFlags::DomainMigrateFlags) -> LibvirtFuture<()> {
         let pl = request::MigratePerformRequest::new(dom, Some(uri), params, vec![], flags);
         Box::new(self.client.request(request::remote_procedure::REMOTE_PROC_DOMAIN_MIGRATE_PERFORM3_PARAMS, pl).map(|resp| {
             println!("DEBUG RESP: {:?}", resp);
